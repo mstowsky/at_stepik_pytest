60a025c0f6f5198dc04bb5d01b1d6f2badf8809c
mstowsky <dflyerggl@gmail.com>
2026-01-30 14:30:36 +0400

    Added xfail mark for expected-failed test (offer7).

--- test_product_page.py
+++ test_product_page.py
@@ -14,9 +14,18 @@ url_offers_list = []
 for i in range(10):
     url_offers_list.append(url_src+url_offer+str(i))
 
+# Записываем заранее известные баги, чтобы проставить им пометку xfail
+xfail_list = ['offer7']
+
 @pytest.mark.parametrize('url', url_offers_list)
 def test_guest_can_add_product_to_basket(browser, url):
     print(f'Start test for offer: {url[-6:]}')
+
+    # Если ссылка из списка известных багов, то проставляем метку xfail
+    for i in xfail_list:
+        if i in url:
+            pytest.xfail(f'Known bug in page \'{i}\'')
+
     page = ProductPage(browser, url)
     page.open()
     page.add_to_cart()
