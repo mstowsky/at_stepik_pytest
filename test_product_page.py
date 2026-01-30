062415260e052550943766f80204fc94e69a4922
mstowsky <dflyerggl@gmail.com>
2026-01-30 14:09:49 +0400

    Added parameters (different offers links) to product page test.

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,22 @@
 import time
+import pytest
 from .pages.product_page import ProductPage
 
-url = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#url1 = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#url2 = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
 
-def test_guest_can_add_product_to_basket(browser):
+url_src = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+url_offer = '?promo=offer'
+url_offers_list = []
+# Формируем список ссылок вида
+# http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offerN
+# где вместо N будут цифры от 0 до 9 включительно
+for i in range(10):
+    url_offers_list.append(url_src+url_offer+str(i))
+
+@pytest.mark.parametrize('url', url_offers_list)
+def test_guest_can_add_product_to_basket(browser, url):
+    print(f'Start test for offer: {url[-6:]}')
     page = ProductPage(browser, url)
     page.open()
     page.add_to_cart()
