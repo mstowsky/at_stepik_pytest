887ba8490140d069ac50db63b30db9aa5a34a7ef
mstowsky <dflyerggl@gmail.com>
2026-04-23 00:39:39 +0400

    Added allure report for 'tests/test_login_page'.

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -8,21 +8,27 @@ def test_guest_can_see_login_form(browser):
     Гость может видеть форму авторизации
     '''
     page = LoginPage(browser, url)
-    page.open()
-    page.should_be_login_form()
+    with allure.step('Шаг 1. Открыть страницу авторизации'):
+        page.open()
+    with allure.step('Шаг 2. Проверить, что страница авторизации отображается'):
+        page.should_be_login_form()
 
 def test_guest_can_see_register_form(browser):
     '''
     Гость может видеть форму регистрации
     '''
     page = LoginPage(browser, url)
-    page.open()
-    page.should_be_register_form()
+    with allure.step('Шаг 1. Открыть страницу авторизации'):
+        page.open()
+    with allure.step('Шаг 2. Проверить, что страница регистрации отображается'):
+        page.should_be_register_form()
 
 def test_login_link_is_correct(browser):
     '''
     Открытая сейчас страница имеет корректную ссылку на страницу авторизации
     '''
     page = LoginPage(browser, url)
-    page.open()
-    page.should_be_login_url()
+    with allure.step('Шаг 1. Открыть страницу авторизации'):
+        page.open()
+    with allure.step('Шаг 2. Проверить, что активна ссылка на страницу авторизации'):
+        page.should_be_login_url()
